01c765f3fa5ef8a4d2132dc868254400d42f92b1
Sharvarivkale <1332sharvarikale@gmail.com>
2026-09-10 11:19:36 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+      
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]==nums[j-1]){
+                return true;
+            }
+        }
+       
+       return false; 
+    }
+};
