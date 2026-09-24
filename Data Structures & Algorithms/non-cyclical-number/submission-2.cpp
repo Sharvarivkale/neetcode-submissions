d71daba8663a1e606381096e20b94b2bb830ab20
Sharvarivkale <1332sharvarikale@gmail.com>
2026-09-24 19:50:12 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int sumofsquare(int n){
+    int sum=0;
+
+    while(n>0){
+        int digit=n%10;
+        sum +=digit*digit;
+        n=n/10;
+    }
+    return sum;
+}
+    bool isHappy(int n) {
+        unordered_set<int> st;
+
+        while(n != 1){
+         if(st.count(n)){
+            return false;
+         }  
+         st.insert(n);
+         n=sumofsquare(n);
+        }
+        return true;
+        
+    }
+};
