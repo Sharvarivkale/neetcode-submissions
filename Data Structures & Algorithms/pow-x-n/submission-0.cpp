9821c36e78a81e318e8262ca35522854aa19ea1b
Sharvarivkale <1332sharvarikale@gmail.com>
2026-09-24 22:47:08 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+    double power(double x, int n){
+        if (n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        double ans=power(x,n/2);
+
+        if(n%2==0){
+            return ans*ans;
+        }
+        else{
+            return x*ans*ans;
+        }
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            return 1 / power(x, -N);
+        }
+        return power(x,  n);
+        
+        
+    }
+};
